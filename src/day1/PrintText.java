7ee8557b9cdbd14ffa396bb14065f476662b5c1a
Rohit Ware <rohitware99@gmail.com>
2026-06-29 23:23:11 +0530

    Simple text print example

---
+++ src/day1/PrintText.java
@@ -0,0 +1,9 @@
+package day1;
+
+public class PrintText {
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+        System.out.println("I am learning Java.");
+        System.out.println("It is awesome!");
+    }
+}

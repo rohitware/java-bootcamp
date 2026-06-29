6c58a28d71315d79db06eaaa10f634169ad22e2d
Rohit Ware <rohitware99@gmail.com>
2026-06-29 23:26:43 +0530

    Simple Print Numbers

---
+++ src/day1/PrintNumbers.java
@@ -0,0 +1,9 @@
+package day1;
+
+public class PrintNumbers {
+    public static void main(String[] args){
+        System.out.println(7);
+        System.out.println(343);
+        System.out.println(750303);
+    }
+}

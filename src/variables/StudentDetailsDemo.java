b706f1f4501c4fd776a1cf26fbdbef3954e56a4d
Rohit Ware <rohitware99@gmail.com>
2026-07-01 00:08:55 +0530

    Add student details example using variables and data types

---
+++ src/variables/StudentDetailsDemo.java
@@ -0,0 +1,19 @@
+package variables;
+
+public class StudentDetailsDemo {
+    public static void main(String[] args){
+
+        String name = "Rohit";
+        int age = 33;
+        String city = "Pune";
+        double percentage = 66.54;
+        boolean isPlaced = true;
+
+        // print
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("City: " + city);
+        System.out.println("Percentage: "+ percentage);
+        System.out.println("Is Placed:" + isPlaced);
+    }
+}

9d054c087b884e3f69d74e5ba3cfe810b0ce32a2
Rohit Ware <rohitware99@gmail.com>
2026-06-30 23:29:11 +0530

    Practice Java variables

---
+++ src/Variables/VariablesDemo.java
@@ -0,0 +1,12 @@
+package Variables;
+
+public class VariablesDemo {
+    public static void main(String[] args){
+        int age = 33;
+        String name = "Rohit";
+
+        System.out.println(age);
+        System.out.println(name);
+    }
+
+}

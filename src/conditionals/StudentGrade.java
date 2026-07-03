efbde8c28bfba01724d741d6da0eea377ff67a18
Rohit Ware <rohitware99@gmail.com>
2026-07-04 01:31:57 +0530

    Practice Java else-if with real-world examples

---
+++ src/conditionals/StudentGrade.java
@@ -0,0 +1,19 @@
+package conditionals;
+
+public class StudentGrade {
+    public static void main(String[] argsS) {
+        int marks = 82;
+
+        if (marks >= 90) {
+            System.out.println("Grade A");
+        } else if (marks >= 75) {
+            System.out.println("Grade B");
+        } else if (marks >= 60) {
+            System.out.println("Grade C");
+        } else if (marks >= 35) {
+            System.out.println("Grade D");
+        } else {
+            System.out.println("Fail");
+        }
+    }
+}

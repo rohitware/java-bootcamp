cf68f4dd196c89dbdfdc439c5e57f07dbd75bffc
Rohit Ware <rohitware99@gmail.com>
2026-07-05 00:39:29 +0530

    Practice Java for loop with real-world examples

---
+++ src/loops/OddNumbers.java
@@ -0,0 +1,9 @@
+package loops;
+
+public class OddNumbers {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 20; i = i + 2) {
+            System.out.println(i);
+        }
+    }
+}
---
+++ src/loops/PrintOneToTwenty.java
@@ -0,0 +1,9 @@
+package loops;
+
+public class PrintOneToTwenty {
+    public static void main(String[] args) {
+        for (int i = 2; i <= 20; i++) {
+            System.out.println(i);
+        }
+    }
+}

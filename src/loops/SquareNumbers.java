d683cf63c8ebb0207a210b0187d13a4875343175
Rohit Ware <rohitware99@gmail.com>
2026-07-05 00:41:44 +0530

    Loop runs from 1 to 10 and prints each squared result

---
+++ src/loops/SquareNumbers.java
@@ -0,0 +1,12 @@
+package loops;
+
+public class SquareNumbers {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 10; i++) {
+            int square = i * i;
+            System.out.println(square);
+        }
+
+    }
+}

f0a8b54fcec255b0c43e27dc7abe981021d3d455
Rohit Ware <rohitware99@gmail.com>
2026-07-06 23:45:27 +0530

    Add EvenNumbersWhile to demonstrate printing even numbers using a while loop

---
+++ src/operators/EvenNumbersWhile.java
@@ -0,0 +1,11 @@
+package operators;
+
+public class EvenNumbersWhile {
+    public static void main(String[] args) {
+        int i = 2;
+        while (i <= 20) {
+            System.out.println(i);
+            i = i + 2;
+        }
+    }
+}

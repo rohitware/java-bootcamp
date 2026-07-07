ac94b05a491050a8b25ae91077c6c74b1894c4d6
Rohit Ware <rohitware99@gmail.com>
2026-07-07 22:53:04 +0530

    Practice Java continue statement

---
+++ src/loops/OddNumbersContinue.java
@@ -0,0 +1,12 @@
+package loops;
+
+public class OddNumbersContinue {
+    public static void main(String[] args) {
+        for (int i = 0; i < 20; i++) {
+            if (i % 2 == 0) {
+                continue;
+            }
+            System.out.println(i);
+        }
+    }
+}

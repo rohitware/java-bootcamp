7fdfdfcbf5ba512ff2b3ea5b5325048b69639aba
Rohit Ware <rohitware99@gmail.com>
2026-07-11 01:31:37 +0530

    Default Constructor example

---
+++ src/constructors/Student.java
@@ -0,0 +1,11 @@
+package constructors;
+
+public class Student {
+    String name;
+    int age;
+
+    // default constructor
+    public Student() {
+        System.out.println("Student object created");
+    }
+}
---
+++ src/constructors/StudentDemo.java
@@ -0,0 +1,7 @@
+package constructors;
+
+public class StudentDemo {
+    public static void main(String[] args) {
+        Student student = new Student();
+    }
+}

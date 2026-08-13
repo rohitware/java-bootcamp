8688d831104f4e2ba343fdc5c058ebba971cb4e1
Rohit Ware <rohitware99@gmail.com>
2026-08-13 23:44:47 +0530

    Add student print in constructor demo

--- src/constructors/StudentDemo.java
+++ src/constructors/StudentDemo.java
@@ -3,5 +3,6 @@ package constructors;
 public class StudentDemo {
     public static void main(String[] args) {
         Student student = new Student();
+        System.out.println(student);
     }
 }
